f75bea8e3f81c7179dd420d5533e7045f57bfd31
Khinshan Khan <shan@uptogood.dev>
2025-11-03 22:44:35 -0500

    simple example checking rdap

--- cmd/gather-cli/main.go
+++ cmd/gather-cli/main.go
@@ -1,7 +1,27 @@
 package main
 
-import ("fmt")
+import (
+	"fmt"
+	"github.com/openrdap/rdap"
+)
+
+var client = &rdap.Client{}
+
+func checkDomainAvailabilityRDAP(domainName string) {
+	domain, err := client.QueryDomain(domainName)
+
+	if err == nil {
+		fmt.Printf("Handle=%s Domain=%s\n", domain.Handle, domain.LDHName)
+	} else {
+		fmt.Printf("Error: %s\n", err)
+	}
+}
 
 func main() {
-	fmt.Println("Hello there!")
+	// taken
+	checkDomainAvailabilityRDAP("khinshankhan.com")
+	// taken but reserved
+	checkDomainAvailabilityRDAP("example.com")
+	// not taken
+	checkDomainAvailabilityRDAP("thisdomaindoesntexistcurrentlybecauseichecked.com")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module github.com/khinshankhan/nomex
 
 go 1.25.3
+
+require (
+	github.com/alecthomas/kingpin/v2 v2.3.2 // indirect
+	github.com/alecthomas/units v0.0.0-20211218093645-b94a6e3cc137 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/openrdap/rdap v0.9.1 // indirect
+	github.com/xhit/go-str2duration/v2 v2.1.0 // indirect
+	golang.org/x/crypto v0.9.0 // indirect
+)
